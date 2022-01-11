a372921835c43bcc6675cae7326615d776416794
Oliver Jones <61389067+OliverjonesDev@users.noreply.github.com>
2022-01-11 23:27:41 +0000

    mosaic update

--- scripts/mosaic.js
+++ scripts/mosaic.js
@@ -14,12 +14,12 @@ function draw() {
     noStroke();
     fill("#E91D50");
     rect((windowWidth / 2) - 625, (windowHeight / 2) - 290, 300, 300);
+    rect((windowWidth / 2) - 600, (windowHeight / 2) + 400, 1500, 200);
     fill(247, 167, 62);
     rect((windowWidth / 2) + 300, (windowHeight / 2) - 180, 300, 450);
+    rect((windowWidth / 2) - 1020, (windowHeight / 2) - 530, 300, 900);
     fill("#4AC4D9");
     rect((windowWidth / 2) - 600, (windowHeight / 2) + 80, 600, 150);
-    fill(247, 167, 62);
-    rect((windowWidth / 2) - 1020, (windowHeight / 2) - 530, 300, 900);
     fill("#4AC4D9");
     rect((windowWidth / 2) + 800, (windowHeight / 2) - 530, 300, 900);
     rect((windowWidth / 2) + 800, (windowHeight / 2) - 300, -400, -230);
